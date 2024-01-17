6bcc8f26a8911ce32c150a05c9309d78fd27477c
bgz <970610450@qq.com>
2024-01-17 13:53:14 +0800

    try idea git commit 3

--- src/main/java/org/example/test1.java
+++ src/main/java/org/example/test1.java
@@ -4,5 +4,6 @@ public class test1 {
     public static void main(String[] args) {
         System.out.println(1234);
         System.out.println(6666);
+        System.out.println(33333333);
     }
 }
